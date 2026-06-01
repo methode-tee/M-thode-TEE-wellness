df91fefeba990024494614111acef37f181587b4
methode-tee <tiana_pondja@outlook.fr>
2026-06-01 15:41:44 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.MT_CONFIG = {
+  SUPABASE_URL: "https://tyuvlmmmyygqqhuetwoe.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dXZsbW1teXlncXFodWV0d29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMDA5NTQsImV4cCI6MjA5NTg3Njk1NH0.zyyFmDqM96TjYSqJW8_bfG330E4jLBheQ_c6qmyQ7W4",
+  STRIPE_CHECKOUT_ENDPOINT: "/.netlify/functions/create-checkout-session",
+  USE_SUPABASE: true,
+  DEMO_MODE: false,
+  ADMIN_EMAILS: ["teayannaparis@gmail.com"],
+  ADMIN_ACCESS_CODE: "OUTITA"
+};
