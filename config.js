457f2add6682c1a19981b0bdb43a1dadf9098589
methode-tee <tiana_pondja@outlook.fr>
2026-06-01 18:57:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 window.MT_CONFIG = {
   SUPABASE_URL: "https://tyuvlmmmyygqqhuetwoe.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dXZsbW1teXlncXFodWV0d29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMDA5NTQsImV4cCI6MjA5NTg3Njk1NH0.zyyFmDqM96TjYSqJW8_bfG330E4jLBheQ_c6qmyQ7W4",
-  STRIPE_CHECKOUT_ENDPOINT: "/.netlify/functions/create-checkout-session",
+
+  SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dXZsbW1teXlncXFodWV0d29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMDA5NTQsImV4cCI6MjA5NTg3Njk1NH0.zyyFmDqM96TjYSqJW8_bfG330E4jLBheQ_c6qmyQ7W4",
+
+  STRIPE_PUBLIC_KEY:
+    "pk_live_51PYP5BHKJ8J9UFkeAAMOhzAIgiwBowwqRqqjEP3MjTTFTROBeKgpF4bPKmYnO0uFSKh6zNAlyXnMDIwkes84JkEJ00sOd94vxH",
+
+  STRIPE_CHECKOUT_ENDPOINT:
+    "https://effervescent-cat-a47e05.netlify.app/.netlify/functions/create-checkout-session",
+
   USE_SUPABASE: true,
+
   DEMO_MODE: false,
+
   ADMIN_EMAILS: ["teayannaparis@gmail.com"],
+
   ADMIN_ACCESS_CODE: "OUTITA"
 };
