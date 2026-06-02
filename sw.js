662da21cd824804514092d28c9453a2afae4bc1c
methode-tee <tiana_pondja@outlook.fr>
2026-06-03 00:41:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
